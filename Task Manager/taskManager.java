05d1a3a593ab8d5fdf46468371bcb1a2601e4d99
Samia Naz <nsamia312@gmail.com>
2026-05-24 21:09:17 +0500

    Implemented undo and redo functionality using two stacks in task manager

--- Task Manager/taskManager.java
+++ Task Manager/taskManager.java
@@ -13,8 +13,9 @@ Node(String data)
 public class taskManager{
     static Scanner sc = new Scanner(System.in);
     static Node top = null;
+    static Node redoTop = null;
     static String  stack ;
-    //push()
+    //push() "Add Task"
     static void push(String data)
     {
         Node newNode = new Node(data);
@@ -23,8 +24,31 @@ public class taskManager{
 
 
     }
+    //RedoPush() "Bring back last task"
+    static void redoPush(String data)
+    {
+        Node newnNode = new Node(data);
+        newnNode.next = redoTop;
+        redoTop = newnNode;
+    } 
+    //redoPop() "Pop and push it to the main Stack "
+    static String redoPop()
+    {
+        if(redoTop == null)
+        {
+            System.out.println("Nothing to redo!");
+            return null;
+        } 
+        else
+        {
+            String value = redoTop.data;
+           // push(value);
+            redoTop = redoTop.next;
+            return value;
+        }
+    }
     //pop()
-    static String pop()
+    static String Undopop()
     {
         if(top == null)
         {
@@ -34,6 +58,7 @@ public class taskManager{
         else
         {
             String data = top.data;
+            redoPush(data);
             top = top.next;
             return data;
         }
@@ -63,9 +88,10 @@ public class taskManager{
         {
             System.out.println("**** TASK MANAGER ****");
             System.out.println("1.Add task");
-            System.out.println("2.Undo task");
+            System.out.println("2.Remove task");
             System.out.println("3.Display task");
-            System.out.println("4.Exit Task");
+            System.out.println("4.Redo task");
+            System.out.println("5.Exit Task");
             System.out.print("Enter your choice : ");
             choice = sc.nextInt();
             sc.nextLine();
@@ -76,20 +102,29 @@ public class taskManager{
                 push(task);
                 break;
                 case 2: 
-                String value = pop();
+                String value = Undopop();
                 System.out.println("The task " + value + " is removed successfully!"); 
                 break;
                 case 3:
                 display();
                 break;
                 case 4:
+                        String value2 = redoPop();
+                        if(value2 != null)
+                        {
+                            push(value2);
+                            System.out.println("Redo: " + value2);
+                        }
+                        break;
+                case 5:
                 System.out.println("Exiting the Menu!");
-                break;
+                break;        
+
                 default:
                 System.out.println("Invalid choice! ");
                 break;
             }
-        }while (choice != 4); 
+        }while (choice != 5); 
             
         
 
